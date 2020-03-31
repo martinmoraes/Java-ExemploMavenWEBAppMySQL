e81cac72b80f0fd839944b16b955e0fb978b4c83
Martin Morães <martin.moraes@gmail.com>
2020-03-31 20:12:19 -0300

    Exemplo de conexão com banco MySQL utilizando Maven

---
+++ src/main/java/com/moraes/CNXJDBC.java
@@ -0,0 +1,30 @@
+package com.moraes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CNXJDBC {
+	
+	private String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+	private String usuario = "desenvolvedor";
+	private String senha = "!qAz@wSx";
+//	private String PathBase = "C:\\Users\\Usuário\\Documents\\Desenvolvimento\\Java\\CalculadoraWEBAPP\\base\\calculos";
+	private String URL = "jdbc:mysql://localhost:3306/avalog?serverTimezone=GMT-3";
+
+	
+	
+	public Connection conectar() {
+		try {
+			Class.forName(DRIVER_CLASS);
+			return DriverManager.getConnection(URL, usuario, senha);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+}
---
+++ src/main/java/com/moraes/Cadastro.java
@@ -0,0 +1,28 @@
+package com.moraes;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Cadastro extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public Cadastro() {
+        super();
+        System.out.println(new CNXJDBC().conectar());
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
